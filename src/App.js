9f8dffdb07d5b3498d6a501201c964d1891b339d
Mosiur <mosiurislamwebdesign@gmail.com>
2022-10-23 21:56:54 +0600

    daisyui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 function App() {
   return (
     <div>
-      
+      <button className="btn">Button</button>
+      <button className="btn btn-primary">Button primary</button>
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-accent">Button</button>
+      <button className="btn btn-ghost">Button</button>
+      <button className="btn btn-link">Button</button>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,31 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#570DF8",
+
+          "secondary": "#F000B8",
+
+          "accent": "#37CDBE",
+
+          "neutral": "#3D4451",
+
+          "base-100": "#FFFFFF",
+
+          "info": "#3ABFF8",
+
+          "success": "#36D399",
+
+          "warning": "#FBBD23",
+
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 }
